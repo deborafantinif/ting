d560bb1b0534fc6d3accf264cb8aa30e595ab4d0
deborafantinif <deboraferfan@gmail.com>
2023-01-02 14:19:51 -0300

    FEAT[06]: lista das linhas onde a palavra foi encontrada no arquivo com o conteudo

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,4 @@
-def search_word(word, file_info, repeat):
+def search_word(word, file_info, repeat, content):
     structure = {
         'palavra': word,
         'arquivo': file_info["nome_do_arquivo"],
@@ -6,20 +6,26 @@ def search_word(word, file_info, repeat):
     }
     for index, item in enumerate(file_info["linhas_do_arquivo"]):
         if word.lower() in item.lower():
-            structure['ocorrencias'].append({"linha": index + 1})
+            if content:
+                structure['ocorrencias'].append({
+                    "linha": index + 1,
+                    "conteudo": item
+                })
+            else:
+                structure['ocorrencias'].append({"linha": index + 1})
     if structure['ocorrencias'] != []:
         repeat.append(structure)
     return repeat
 
 
-def exists_word(word, instance):
+def structure_word(word, instance, content):
     verify = True
     index = 0
     repeat = []
     try:
         while verify:
             file_info = instance.search(index)
-            repeat = search_word(word, file_info, repeat)
+            repeat = search_word(word, file_info, repeat, content)
             index += 1
     except IndexError:
         if index == 0:
@@ -28,5 +34,9 @@ def exists_word(word, instance):
         return repeat
 
 
+def exists_word(word, instance):
+    return structure_word(word, instance, False)
+
+
 def search_by_word(word, instance):
-    return []
+    return structure_word(word, instance, True)
