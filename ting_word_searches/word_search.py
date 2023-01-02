24438e0ebe9b180415bdddfc6556dbffbfe33e05
deborafantinif <deboraferfan@gmail.com>
2023-01-02 14:10:53 -0300

    FEAT[06]: lista das linhas onde a palavra foi encontrada no arquivo

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,32 @@
+def search_word(word, file_info, repeat):
+    structure = {
+        'palavra': word,
+        'arquivo': file_info["nome_do_arquivo"],
+        'ocorrencias': []
+    }
+    for index, item in enumerate(file_info["linhas_do_arquivo"]):
+        if word.lower() in item.lower():
+            structure['ocorrencias'].append({"linha": index + 1})
+    if structure['ocorrencias'] != []:
+        repeat.append(structure)
+    return repeat
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    verify = True
+    index = 0
+    repeat = []
+    try:
+        while verify:
+            file_info = instance.search(index)
+            repeat = search_word(word, file_info, repeat)
+            index += 1
+    except IndexError:
+        if index == 0:
+            return []
+        verify = False
+        return repeat
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return []
