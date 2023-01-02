a6bd67387d8747e882432fbe13f663b31eb689a6
deborafantinif <deboraferfan@gmail.com>
2023-01-02 12:43:11 -0300

    FEAT[04]: remoção das infos dos arquivos na fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,7 +17,13 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        first_file = instance.search(0)
+    except:
+        sys.stdout.write('Não há elementos\n')
+        return None
+    instance.dequeue()
+    sys.stdout.write(f'Arquivo {first_file["nome_do_arquivo"]} removido com sucesso\n')
 
 
 def file_metadata(instance, position):
