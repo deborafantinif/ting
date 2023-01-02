a0c6aefb399a4459d2c2736c6b63be40accb29f8
deborafantinif <deboraferfan@gmail.com>
2023-01-02 11:33:57 -0300

    FEAT[02]: leitura de arquivo txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+from os import path
+import sys
+
 def txt_importer(path_file):
-    pass
+    if not path.exists(path_file):
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+        return None
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido\n')
+    with open(path_file, 'r') as text:
+        return [line.rstrip() for line in text]
