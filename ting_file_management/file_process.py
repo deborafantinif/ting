ef6db3f9eae1b0f14228c9ec2b611a8e1f105318
deborafantinif <deboraferfan@gmail.com>
2023-01-02 12:22:15 -0300

    FEAT[03]: inserindo infos do arquivo dentro da fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list_text = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_text),
+        "linhas_do_arquivo": list_text
+    }
+    repeat = [1 for x in instance.queue if x["nome_do_arquivo"] == path_file]
+    if len(repeat) > 0:
+        return None
+    instance.enqueue(new_dict)
+    sys.stdout.write(str(new_dict))
 
 
 def remove(instance):
