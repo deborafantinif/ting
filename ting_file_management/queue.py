20420e0a4ff72860df73b62c4b7f190cd31f5150
deborafantinif <deboraferfan@gmail.com>
2023-01-02 10:59:45 -0300

    FEAT[01]: classe de manipulação de fila

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) == 0:
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        n_elements = len(self.queue) - 1
+        if index > n_elements or index < 0:
+            raise IndexError('list index out of range')
+        return self.queue[index]
